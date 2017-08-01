afaa1e0e71ae9d0be97a619d2a712fadf7a3a221
夏侯暮羽 <zyf6@outlook.com>
2017-08-01 01:03:05 -0400

    Time
    Add a time show.

--- Solve/Prime.cpp
+++ Solve/Prime.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<math.h>
+#include<time.h>
 int main()
 {
     int Num,S,T;
@@ -9,13 +9,14 @@ int main()
     printf("And then, we can show you the prime found by us:\n");
     for(int S=1;S<=Num;S++)
     {
-        for(int n=1;n<=1000;n++)
+        for(int n=1;n<=Num*10;n++)
         {
             if(S % n==0)T++;
         }
         if(T==2)printf("%d | ",S);
         T=0;
     }
+    printf("Time used=%.2f\n", (double)clock()/CLOCKS_PER_SEC);
     getchar();
     return 0;
 }
